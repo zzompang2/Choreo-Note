227ac6cf5b8a4d5b5a2036afdaea8dde4d3a6431
함창수 <ham@hamchangsuui-MacBook-Pro.local>
2020-08-09 22:09:29 +0900

    stack navigation

---
+++ src/main/FormationScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+	SafeAreaView, Text,
+} from 'react-native';
+
+export default class FormationScreen extends React.Component {
+	
+	render() {
+		return(
+			<SafeAreaView>
+					<Text>hellow ham!</Text>
+			</SafeAreaView>
+		)
+	}
+}
---
+++ src/main/ListScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {
+  SafeAreaView, Text, 
+} from 'react-native';
+
+export default class ListScreen extends React.Component {
+	render() {
+		return(
+			<SafeAreaView>
+				<Text>goodbye ham</Text>
+			</SafeAreaView>
+		)
+	}
+}
