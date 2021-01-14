aa2d3f2fe64569a4fc020f8c4b120d7226be4a80
함창수 <ham@hamchangsuui-MacBook-Pro.local>
2021-01-14 19:45:11 +0900

    프로젝트 초기화

--- App.js
+++ App.js
@@ -1,11 +1,7 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import ScreenStack from './src/screens';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <ScreenStack/>
-    </NavigationContainer>
+    null
   );
 };
--- src/values/Fonts.js
+++
@@ -1,9 +0,0 @@
-export const FONTS = {
-  BM: "BMEULJIROTTF",
-  February: 'February',
-  binggrae: 'Binggrae',
-  binggrae_bold: 'Binggrae-Bold',
-  binggrae2: 'binggrae2',
-  binggrae2_bold: 'binggrae2_bold',
-  aAlleyGarden: 'aAlleyGarden',
-}
