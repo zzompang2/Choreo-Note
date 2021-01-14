887bb075cacd862a9a7ad8d5207dad78cf2de89f
함창수 <ham@hamchangsuui-MacBook-Pro.local>
2021-01-14 22:54:37 +0900

    SQLite 사용/ note list 출력

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -1,11 +1,80 @@
 import React from 'react';
 import {
-  SafeAreaView, Text, TouchableOpacity
+  SafeAreaView, Text, TouchableOpacity, FlatList, View
 } from 'react-native';
+import SQLite from "react-native-sqlite-storage";
+
+const db = SQLite.openDatabase({ name: 'ChoreoNote.db' });
 
 export default class ListScreen extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			noteList: []
+		};
+
+		this.getDatabaseData();
+	}
+
+	getDatabaseData() {
+		const { noteList } = this.state;
+
+		db.transaction(txn => {
+			/*=== 기존 table 초기화 ===*/
+			txn.executeSql('DROP TABLE IF EXISTS note');
+
+			// table 없으면 생성
+			txn.executeSql(
+				'CREATE TABLE IF NOT EXISTS note(' +
+				'nid INTEGER NOT NULL, ' +
+				'title TEXT NOT NULL, ' +
+				'date TEXT NOT NULL, ' +
+				'PRIMARY KEY("nid") );'
+			);
+
+			// DB 에서 note 정보 가져오기
+			txn.executeSql(
+				"SELECT * FROM note", 
+				[],
+        (txn, result) => {
+					// 노트가 없는 경우: default note 추가
+					if(result.rows.length == 0) {
+						const title = 'Choreo Note에 오신걸 환영해요!';
+						const date = this.dateFormat(new Date());
+
+						txn.executeSql(
+							"INSERT INTO note VALUES (0, ?, ?);",
+							[title, date],
+							() => {
+								noteList.push({nid: 0, title, date});
+								this.setState({ noteList });
+							}
+						);
+					}
+					// note 정보 가져오기
+					else {
+						for (let i = 0; i < result.rows.length; i++)
+							noteList.push(result.rows.item(i));
+						this.setState({ noteList });
+					}
+				}
+			);
+		},
+		e => console.log("DB ERROR", e),
+		() => console.log("DB SUCCESS"));
+	}
+
+	/* Date() 로 받은 값을 YYYY.MM.DD 포멧의 string 으로 변경 */
+	dateFormat(date) {
+		return date.getFullYear() + "." + 
+					(date.getMonth() < 9 ? '0' + (date.getMonth()+1) : date.getMonth()+1) + 
+					"." + date.getDate();
+	}
 
 	render() {
+		const { noteList } = this.state;
+
 		return(
 			<SafeAreaView>
 				<Text>ListScreen</Text>
@@ -15,6 +84,16 @@ export default class ListScreen extends React.Component {
 				}}>
 					<Text>go to FormationScreen</Text>
 				</TouchableOpacity>
+				<FlatList
+				data={noteList}
+				keyExtractor={(item, idx) => idx.toString()}
+				renderItem={({ item }) =>
+					<View style={{flexDirection: 'row'}}>
+						<Text numberOfLines={1}>{item.nid}</Text>
+						<Text numberOfLines={1}>{item.title}</Text>
+						<Text numberOfLines={1}>{item.date}</Text>
+					</View>
+				} />
 			</SafeAreaView>
 		)
 	}
